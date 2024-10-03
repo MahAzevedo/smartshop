2e78564e7246d2c6638792c0d198ca759d890fa5
MahAzevedo <madavetto@gmail.com>
2024-10-03 19:31:16 -0300

    src/main.jsx > src/index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+
+createRoot(document.getElementById('root')).render(
+  // <StrictMode>
+  <App />
+  // </StrictMode>,
+);
+// deletar o <StrictMode> pq ele resnderiza duas vezes e isso atrapalha , aqui eu só comitei
