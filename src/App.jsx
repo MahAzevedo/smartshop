d590814e0a1a295c9630b68e04af3068a76feced
MahAzevedo <madavetto@gmail.com>
2024-10-05 20:17:44 -0300

    modified:   index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Produtos from './Components/Produtos';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 const App = () => {
-  return <div>App React</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Produtos />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
+
+// abrir react loaging
--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,6 @@ import App from './App.jsx';
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
   <App />
-  // </StrictMode>,
+  // </StrictMode>
 );
 // deletar o <StrictMode> pq ele resnderiza duas vezes e isso atrapalha , aqui eu só comitei
