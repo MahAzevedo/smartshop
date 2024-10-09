8725eab2ebbd2d6174fa813c3a35b986d30f00d6
MahAzevedo <madavetto@gmail.com>
2024-10-09 00:01:08 -0300

    modified:   src/index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+// import { StrictMode } from 'react';
+// import { createRoot } from 'react-dom/client';
+// import App from './App';
 
-createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-  <App />
-  // </StrictMode>
-);
-// deletar o <StrictMode> pq ele resnderiza duas vezes e isso atrapalha , aqui eu só comitei
+// ReactDOM.createRoot(document.getElementById('root')).render(
+// <StrictMode>
+// <App />
+// </StrictMode>
+// );
+// deletar o <StrictMode> pq ele renderiza duas vezes e isso atrapalha , aqui eu só comitei
+
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
