4a41c61baf1ca01f0a63baceb1d8d6977a49394f
MahAzevedo <madavetto@gmail.com>
2024-10-12 18:23:53 -0300

    modified: src/Components/Contato.jsx   &   modified: src/Components/Head.jsx

--- src/Components/Head.jsx
+++ src/Components/Head.jsx
@@ -4,7 +4,7 @@ const Head = (props) => {
   React.useEffect(() => {
     document.title = props.title;
     document
-      .querySelector('meta [name="description"]')
+      .querySelector('meta[name="description"]')
       .setAttribute('content', props.description);
   }, [props]);
 
