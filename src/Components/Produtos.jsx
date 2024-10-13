e900c3e25eeb85af94a196e4df8c77daef5c64fe
MahAzevedo <madavetto@gmail.com>
2024-10-13 19:04:16 -0300

    modified: src/Components/Produtos.jsx

--- src/Components/Produtos.jsx
+++ src/Components/Produtos.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './Produtos.module.css';
 
 const Produtos = () => {
+  React.useEffect(() => {}, []);
   return (
     <div className={styles.produtos}>
       <h1>Produtos</h1>
