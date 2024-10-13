e4c873f2b9efcc78d7666661f9abac7f38b88a03
MahAzevedo <madavetto@gmail.com>
2024-10-13 19:10:08 -0300

    modified:   src/Components/Produtos.jsx  -->> then() json

--- src/Components/Produtos.jsx
+++ src/Components/Produtos.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import styles from './Produtos.module.css';
 
 const Produtos = () => {
-  React.useEffect(() => {}, []);
+  React.useEffect(() => {
+    fetch('https://ranekapi.origamid.dev/json/api/produto').then(r => r.json()).;
+  }, []);
   return (
     <div className={styles.produtos}>
       <h1>Produtos</h1>
