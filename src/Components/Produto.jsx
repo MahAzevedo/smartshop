4e1e2b02b1da38d1512a7e77611b09c0d8424bbc
MahAzevedo <madavetto@gmail.com>
2024-10-15 21:37:50 -0300

    modified:   src/Components/Produto.jsx / produto

--- src/Components/Produto.jsx
+++ src/Components/Produto.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import styles from './Produto.module.css';
+import { useParams } from 'react-router-dom';
 
 const Produto = () => {
+  const { id } = useParams();
+
   return (
     <div className={styles.produto}>
       <h1>Produto</h1>
